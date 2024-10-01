dd5847a2927d05146978b1afbb568ddd438d1b61
Paulo André <153298446+pawl12s@users.noreply.github.com>
2024-10-01 10:36:05 -0400

    aaaaa

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,25 @@
-// LIVE SERVER é do FRONT-END
-// QUEM É O LIVE SERVER DO BACK-END?
-
-// 1 - Para construir um servidor back-end e responder
-// Vamos utilizar o EXPRESS
 import express from 'express'
-import cors from 'cors'
 import mysql from 'mysql2/promise'
-//Criar um objeto do tipo express.
 const app = express()
-//incluir pra ele receber json
-app.use(express.json())  //Middleware
-//incluir o CORS -> QUANDO A GENTE TEM OUTRA PORTA FAZENDO REQUISIÇÃO PARA A PORTA DO SERVIDOR
-app.use(cors())
-//ROTAS
-app.get("/produtos",async(req,res)=>{
-
-    //O que eu tenho que fazer aqui dentro?
-    //OK -> PASSO 1: Criar o banco de dados
-    //PASSO 2: Usar a lib mysql2 para conectar com o banco
+app.get("/produtos", async(req,res)=>{
+    //OK -> 0 - Criar o banco de dados e iniciar o servidor de banco.
+    //1 - Criar a conexão com o banco
     try{
-        const conexao = await mysql.createConnection({
-            host: process.env.dbhost?process.env.dbhost:"localhost",
+        const conection = await mysql.createConnection({
+            host:process.env.dbhost?process.env.dbhost:"localhost",
             user:process.env.dbuser?process.env.dbuser:"root",
             password:process.env.dbpassword?process.env.dbpassword:"",
             database:process.env.dbname?process.env.dbname:"banco1022a",
             port:process.env.dbport?parseInt(process.env.dbport):3306
         })
-        //PASSO 3: QUERY  -> SELECT * FROM produtos
-        const [result,fields]  = await conexao.query("SELECT * FROM produtos")
-        //PASSO 4: Colocar os dados do banco no response
+        //2 - Realizar uma consulta na tabela
+        const [result, fields] = await conection.query("SELECT * from produtos")
+        //3 - Devolver os dados pra quem pediu
         res.send(result)
     }catch(e){
-        res.status(500).send("Erro do servidor")
+        res.status(500).send("Server ERROR")
     }
-    
-    
 })
-
-
-//INICIAR O SERVIDOR
 app.listen(8000,()=>{
-    console.log("SERVIDOR INICIADO NA PORTA 8000")
+    console.log("Iniciei o servidor")
 })
